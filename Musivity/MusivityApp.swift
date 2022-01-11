5bb38c918809c0cb90411da4db2ab2d01fbe5514
tillieferguson <tillieferguson26@gmail.com>
2022-01-11 11:02:09 -0500

    Initial Commit

---
+++ Musivity/MusivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusivityApp.swift
+//  Musivity
+//
+//  Created by Tillie Ferguson on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
